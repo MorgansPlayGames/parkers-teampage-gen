d1a181c287217441fc253fff50c817d75bcd0e2f
Parker Morgan <jpmaster13@msn.com>
2020-11-21 21:50:10 -0700

    hopfully final update, deleting excess code.

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # parkers-teampage-gen
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+[Link to repository]()
+
+[Video of parkers-teampage-gen in action]()
+
+1. [ Description. ](#description)
+2. [ Installation ](#installation-instructions)
+3. [ Usage Information ](#usage-information)
+4. [ Contribution Guidelines ](#contribution-guidelines)
+5. [ Licence ](#licence)
+6. [ Questions ](#questions?)
+
+## Description:
 Creates a webpage that displays employees and their information.
+
+### Installation Instructions
+    Download
+    npm install
+    node app.js
+ 
+### Usage Information
+    On run, User is asked questions on:
+    * manager
+    * name, id, email, office number
+    * if they want to add an employee
+    * engineer
+    * name, id, email, github
+    * intern
+    * name id email school
+    Then an html is created with all the input information put into cards
+
+### Contribution Guidelines
+    Fork, update, and
+    Submit your code to me!
+
+### Licence 
+    Copyright (c) 2020, Parker Morgan
+    All rights reserved.
+        
+    This source code is licensed under the MIT-style license found in the
+    LICENSE file in the root directory of this source tree.
+
+### Questions?
+    GitHub: MorgansPlayGames
+    Email: jpmaster13@msn.com
--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 let questionIndex = 'newPage';
-let quitCheck = true;
 let employeeType = 'manager'
 let employeeList = []
 let currentEmployee = {}
@@ -35,66 +34,56 @@ function question(ask){
 
 //main question logic function what to do with answers passed in
 function theSwitch(answers){
-    if(quitCheck != false){
-        //what to do with the response of:
-        switch(questionIndex){
-            case 'newPage':
-                if(answers.newPage === true){
-                    questionIndex = 'managerCheck';
-                    question(q.managerCheck);
-                //quit out of loop
-                }else onEnd();
-                break;
-            case 'managerCheck':
-                if(answers.manager === true){
-                    questionIndex = 'newEmployee';
-                    question(q.manager);
-                //quit out of loop
-                }else onEnd();
-                break;
-            //runs over the questions for filling out the html
-            case 'newEmployee':
-                //creates a new class with the new employee info passed in
-                if(employeeType === 'manager'){
-                    currentEmployee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-                }else if(employeeType === 'engineer'){
-                    currentEmployee = new Engineer(answers.name, answers.id, answers.email, answers.github);
-                }else if(employeeType === 'intern'){
-                    currentEmployee = new Intern(answers.name, answers.id, answers.email, answers.school);
-                }
-                employeeList.push(currentEmployee);
-                //employee check passed down from newEmployee
-                if(answers.newEmployee === true){
-                    questionIndex = 'employeeType'
-                    question(q.employeeType);
-                //quit out of loop
-                }else onEnd();
-                break;
-            case 'employeeType':
-                //checks the type and gets the questions of the type
-                if(answers.employeeType === 'Engineer'){
-                    question(q.engineer);
-                    employeeType = 'engineer';
-                }else{
-                    question(q.intern);
-                    employeeType = 'intern';
-                }
-                questionIndex = 'newEmployee'
-                break;
-        }      
-    //on finish  
-    }
+    //what to do with the response of:
+    switch(questionIndex){
+        case 'newPage':
+            if(answers.newPage === true){
+                questionIndex = 'managerCheck';
+                question(q.managerCheck);
+            //quit out of loop
+            }else onEnd();
+            break;
+        case 'managerCheck':
+            if(answers.manager === true){
+                questionIndex = 'newEmployee';
+                question(q.manager);
+            //quit out of loop
+            }else onEnd();
+            break;
+        //runs over the questions for filling out the html
+        case 'newEmployee':
+            //creates a new class with the new employee info passed in
+            if(employeeType === 'manager'){
+                currentEmployee = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+            }else if(employeeType === 'engineer'){
+                currentEmployee = new Engineer(answers.name, answers.id, answers.email, answers.github);
+            }else if(employeeType === 'intern'){
+                currentEmployee = new Intern(answers.name, answers.id, answers.email, answers.school);
+            }
+            employeeList.push(currentEmployee);
+            //employee check passed down from newEmployee
+            if(answers.newEmployee === true){
+                questionIndex = 'employeeType'
+                question(q.employeeType);
+            //quit out of loop
+            }else onEnd();
+            break;
+        case 'employeeType':
+            //checks the type and gets the questions of the type
+            if(answers.employeeType === 'Engineer'){
+                question(q.engineer);
+                employeeType = 'engineer';
+            }else{
+                question(q.intern);
+                employeeType = 'intern';
+            }
+            questionIndex = 'newEmployee'
+            break;
+    }       
 }
 //start the loop on script start
 question(q.newPage);
 
-//test function
-function here(){
-    console.log('here');
-}
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 function onEnd(){
     console.log(employeeList);
     rend = render(employeeList)
@@ -103,18 +92,3 @@ function onEnd(){
         console.log('success')
     })
 }
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
